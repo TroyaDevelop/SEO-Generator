bebf87c7daba3c86b6a23249b5906d54e5460c81
TroyaDevelop <goddys777@icloud.com>
2025-09-16 18:54:31 +0300

    fix

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-const port = 3001;
+const port = 3000;
 
 
 app.use(cors());
