6cad8f915ea9bd18383c50a832433685732f568e
TroyaDevelop <goddys777@icloud.com>
2025-09-19 14:38:38 +0300

    update 0.3 fix

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ import taskRoutes from './routes/taskRoutes.js';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-const port = 3001;
+const port = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
